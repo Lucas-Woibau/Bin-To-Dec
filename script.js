45039e0da0368d8413c7369cec6c2baea55b55c5
Lucas Woibau <lucaswoibau7@gmail.com>
2023-05-04 16:20:22 -0300

    Revertendo Número para aplicar o cálculo

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let numBinario = "";
-let saida = "";
+let numBin = '';
+let saida = '';
 
 function Converter(){
-    numBinario = document.getElementById('numBin').value;
-    saida = document.getElementById('numDec').value = numBinario;
+    numBin = document.getElementById('numBin').value;
+    const numBinStr = numBin.toString();          //Convertendo para String
+
+    for(var i = 0; i < numBin.length; i++){     //Dividindo numero em individuais
+        numBin = numBinStr.split('');
+        if(numBin[i] != 0 && numBin[i] != 1){
+            return ExibirAlerta();
+        }
+    }
+
+    //Fazer o cálculo
+    let reverseNumBin = numBin.reverse().join('');
+
+    saida = document.getElementById('numDec').value = reverseNumBin;
+    
+}
+
+function ExibirAlerta(){
+    alert("Digite apenas 0s e 1s!")
 }
