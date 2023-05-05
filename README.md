269da75de7a4cf867d9c7d442fce6e0dbf7ff6a0
Lucas Woibau <lucaswoibau7@gmail.com>
2023-05-05 11:07:56 -0300

    Finalizado

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 ## Tecnologias usadas:
 
 ### HTML5 | CSS 3 | JAVASCRIPT
+
+### O usuário pode inserir até 8 ou mais dígitos binários em um campo de entrada.
+### O usuário é notificado se algo diferente de um 0 ou 1 foi inserido.
+### É exibido para o usuário os resultados em um único campo de saída contendo o equivalente decimal (base 10) do número binário que foi inserido.
+### O resultado pode ser copiado para área de transferência (Bônus)
--- script.js
+++ script.js
@@ -3,22 +3,45 @@ let saida = '';
 
 function Converter(){
     numBin = document.getElementById('numBin').value;
-    const numBinStr = numBin.toString();          //Convertendo para String
+    const numBinStr = numBin.toString()       
 
-    for(var i = 0; i < numBin.length; i++){     //Dividindo numero em individuais
-        numBin = numBinStr.split('');
+    //Dividindo numero em individuais
+    for(var i = 0; i < numBin.length; i++){     
+        numBin = numBinStr.split('')
         if(numBin[i] != 0 && numBin[i] != 1){
-            return ExibirAlerta();
+            return ExibirAlerta()
         }
     }
 
     //Fazer o cálculo
-    let reverseNumBin = numBin.reverse().join('');
+    let finalNumberDec = []
+    numBin.reverse()
 
-    saida = document.getElementById('numDec').value = reverseNumBin;
+    for(var i = 0; i < numBin.length; i++){
+        const calcExpo = Math.pow(2, 0 + i)
+        finalNumberDec.push(calcExpo * numBin[i])      
+    }
+
+    //Somando os números
+    let somaFinalNumberDec = finalNumberDec.reduce((acumulador, valorAtual) => acumulador + valorAtual,0)
+    
+    saida = document.getElementById('numDec').value = somaFinalNumberDec;
     
 }
 
 function ExibirAlerta(){
     alert("Digite apenas 0s e 1s!")
+}
+
+function CopiarTexto(){
+    const input = document.getElementById('botao-copiar')
+
+    navigator.clipboard.writeText(saida.valueOf())
+
+    .then(() => {
+        alert("Texto copiado para área de transferência!")
+    })
+    .catch((error) =>{
+        console.error("Erro ao copiar o texto: ",error)
+    })
 }
